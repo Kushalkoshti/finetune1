1e5b3207e9206bf799840ce01d2ef7f0f036f76c
Kushalkoshti <kushalkoshti10@gmail.com>
2023-03-28 10:31:58 +0200

    first

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("Hello")
+from transformers import AutoModelWithLMHead, AutoTokenizer
+import torch
+
+# print("cuda" if torch.cuda.is_available() else "cpu")
+
+tokenizer = AutoTokenizer.from_pretrained("tuner007/t5_abs_qa")
+model = AutoModelWithLMHead.from_pretrained("tuner007/t5_abs_qa")
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model = model.to(device)
+
+def get_answer(question, context):
+    input_text = "context: %s <question for context: %s </s>" % (context,question)
+    features = tokenizer([input_text], return_tensors='pt')
+    out = model.generate(input_ids=features['input_ids'].to(device), attention_mask=features['attention_mask'].to(device))
+    return tokenizer.decode(out[0])
+
+context = "In Norse mythology, Valhalla is a majestic, enormous hall located in Asgard, ruled over by the god Odin."
+question = "What is Valhalla?"
+get_answer(question, context)
